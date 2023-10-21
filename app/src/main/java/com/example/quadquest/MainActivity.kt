6f557b94010a54fe5b4a76a3388b1f804a1e048b
Ritwik <rbabu1@unh.newhaven.edu>
2023-10-21 01:07:04 -0400

    Tried fragment navigation from splash (**not working)
    tried to redirect from splash fragment to intro sliders (using handler, after set delay) but navigation graph isn't working, even though the controller id is set correctly

--- app/src/main/java/com/example/quadquest/MainActivity.kt
+++ app/src/main/java/com/example/quadquest/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.quadquest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.quadquest.databinding.ActivityMainBinding
+import com.example.quadquest.databinding.FragmentSplashBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var splashBinding: FragmentSplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+        splashBinding = FragmentSplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+
     }
 }
--- app/src/main/java/com/example/quadquest/splash/IntroSlider1Fragment.kt
+++ app/src/main/java/com/example/quadquest/splash/IntroSlider1Fragment.kt
@@ -6,28 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.quadquest.R
+import com.example.quadquest.databinding.FragmentIntroSlider1Binding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [IntroSlider1Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class IntroSlider1Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var binding: FragmentIntroSlider1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +19,8 @@ class IntroSlider1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro_slider1, container, false)
+        binding = FragmentIntroSlider1Binding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment IntroSlider1Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            IntroSlider1Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
