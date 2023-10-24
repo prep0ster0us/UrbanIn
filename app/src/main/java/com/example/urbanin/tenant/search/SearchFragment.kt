3ed24e50fbcf6352a347b3a299c49627787ae8e2
Ritwik <rbabu1@unh.newhaven.edu>
2023-10-24 18:41:09 -0400

    Fixed Maps API dependencies
    added in missed out dependencies for google maps API and corrected boiler-plate code for displaying landing view for Maps

--- app/src/main/java/com/example/urbanin/tenant/search/SearchFragment.kt
+++ app/src/main/java/com/example/urbanin/tenant/search/SearchFragment.kt
@@ -7,24 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.urbanin.databinding.FragmentSearchBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class SearchFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
+    // google map object (for callback)
     private lateinit var googleMap: GoogleMap
 
-    private val binding get() = _binding!!
     private lateinit var listingView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding.listingMapView.onCreate(savedInstanceState)
-        binding.listingMapView.onResume()
-
-        binding.listingMapView.getMapAsync(this)
     }
 
     override fun onCreateView(
@@ -35,9 +35,20 @@ class SearchFragment : Fragment(), OnMapReadyCallback {
         return FragmentSearchBinding.inflate(layoutInflater).root
     }
 
-    override fun onMapReady(map: GoogleMap) {
-        map.let{
-            googleMap = it
+    // initialize map fragment and add async function callback (to avoid onCreate error if map takes time to load and onCreate() is requesting before it's available)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(binding.listingMapView.id) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    // set map callback for google maps (i.e. set position and initial manifest properties
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.let{
+            val newHaven = LatLng(41.293011385559716, -72.96167849997795)
+            googleMap.addMarker(MarkerOptions().position(newHaven).title("Marker on University"))
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(newHaven))
         }
     }
 
