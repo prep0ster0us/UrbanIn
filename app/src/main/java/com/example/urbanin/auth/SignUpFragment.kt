681690ba0461e4b2cfb8fa765326826d88a7a520
Ritwik <rbabu1@unh.newhaven.edu>
2023-10-25 17:01:21 -0400

    Completed basic sign up page layout

--- app/src/main/java/com/example/urbanin/auth/SignUpFragment.kt
+++ app/src/main/java/com/example/urbanin/auth/SignUpFragment.kt
@@ -1,33 +1,38 @@
 package com.example.urbanin.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.urbanin.R
+import com.example.urbanin.databinding.FragmentSignUpBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private lateinit var signUpBinding: FragmentSignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        signUpBinding = FragmentSignUpBinding.inflate(layoutInflater)
+
+        // Configure sign-in to request the user's ID, email address, and basic profile.
+        // ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()// Build a GoogleSignInClient with the options specified by gso.
+
+//        var mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signUpBinding.signUpViewGoogle.setOnClickListener{
+            // https://developers.google.com/identity/sign-in/android/sign-in
+            // TODO: Fetch click listener code for Google Sign In API
         }
+
+
     }
 
     override fun onCreateView(
@@ -38,23 +43,13 @@ class SignUpFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+//        val account = GoogleSignIn.getLastSignedInAccount(this)
+//        updateUI(account)
     }
+
 }
