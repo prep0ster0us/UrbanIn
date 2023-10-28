417f58d74f39fb40c743cb5b1cd55199538b3c11
Ritwik <rbabu1@unh.newhaven.edu>
2023-10-28 00:50:51 -0400

    Created bottom nav bar navigation controller

--- app/src/main/java/com/example/urbanin/MainActivity.kt
+++ app/src/main/java/com/example/urbanin/MainActivity.kt
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.urbanin.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
-    private var db = FirebaseFirestore.getInstance()
-
+//    private var db = FirebaseFirestore.getInstance()
     companion object {
         var TAG = "UrbanIn-Debug"
     }
@@ -36,5 +44,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+
+        // set navigation within bottom nav bar
+        val navView: BottomNavigationView = binding.bottomNavbar
+
+        val navController = findNavController(binding.fragmentContainerView.id)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navSearch,
+                R.id.navSaved,
+                R.id.navMsg,
+                R.id.navAcct
+            )
+        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
