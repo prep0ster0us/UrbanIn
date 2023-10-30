a88b9855c165244a125b4b564691d38aab29f102
nehasingam <nsingam11@gmail.com>
2023-10-30 16:34:20 -0400

    added activity selection fragment and changed navigation a bit

--- app/src/main/java/com/example/urbanin/splash/IntroSlide4Fragment.kt
+++ app/src/main/java/com/example/urbanin/splash/IntroSlide4Fragment.kt
@@ -2,12 +2,11 @@ package com.example.urbanin.splash
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.urbanin.MainActivity
-import com.example.urbanin.R
 import com.example.urbanin.databinding.FragmentIntroSlide4Binding
 
 class IntroSlide4Fragment : Fragment() {
