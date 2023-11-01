c07ee774428bc9954f7cad6f1592f931a2e6a54f
Ritwik <rbabu1@unh.newhaven.edu>
2023-10-31 23:32:58 -0400

    Template code for Firestore authentication
    Basic boiler plate code for signing in using customer credentials in Firestore; **move code for checking if a user is already signed in, to Navigation Activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-firestore-ktx:24.9.1")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
