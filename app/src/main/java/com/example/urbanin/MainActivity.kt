06ab539dba5b92a289bf6c125823b22d0f5ede57
Ritwik <rbabu1@unh.newhaven.edu>
2023-11-02 20:30:25 -0400

    Test commit

--- app/src/main/java/com/example/urbanin/MainActivity.kt
+++ app/src/main/java/com/example/urbanin/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+// test
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
