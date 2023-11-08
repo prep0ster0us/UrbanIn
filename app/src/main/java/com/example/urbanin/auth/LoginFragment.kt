21e8bf48060b63f693112cdfe976004d31543460
Ritwik <rbabu1@unh.newhaven.edu>
2023-11-08 17:34:47 -0500

    set up sign in XML

--- app/src/main/java/com/example/urbanin/auth/LoginFragment.kt
+++ app/src/main/java/com/example/urbanin/auth/LoginFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import com.example.urbanin.LandlordActivity
 import com.example.urbanin.R
 import com.example.urbanin.databinding.FragmentLoginBinding
 
@@ -26,11 +29,14 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.loginViewSubmitBtn.setOnClickListener {
-            if (isAdded) {
+//            if (isAdded) {
+            if (true) {
                 // Add your login logic here
 
                 // Navigate to LandlordFragment
-                findNavController().navigate(R.id.action_loginFragment_to_landlordFragment)
+                val navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+                val navController = navHostFragment.navController
+                navController.navigate(R.id.navigate_login_to_landlord_search)
             }
         }
 
