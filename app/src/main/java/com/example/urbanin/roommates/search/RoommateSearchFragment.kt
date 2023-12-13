491fafaf265986758099d2d42cc2382dfe875db5
prep0ster0us <rbabu1@unh.newhaven.edu>
2023-12-13 16:45:08 -0500

    added roommate search sort

--- app/src/main/java/com/example/urbanin/roommates/search/RoommateSearchFragment.kt
+++ app/src/main/java/com/example/urbanin/roommates/search/RoommateSearchFragment.kt
@@ -12,8 +12,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.urbanin.data.RoommateListAdapter
 import com.example.urbanin.data.RoommateListingData
 import com.example.urbanin.data.SearchListingUtil
+import com.example.urbanin.data.sortOptions
 import com.example.urbanin.databinding.FragmentRoommateSearchBinding
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
@@ -29,6 +31,17 @@ class RoommateSearchFragment : Fragment(), RoommateListAdapter.Callbacks {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: RoommateListAdapter
 
+    // for sort
+    private var sortOptions = arrayOf("Latest", "Budget", "Room Size", "Age", "Name")
+    private var sortMap = linkedMapOf(
+        "Latest" to "datePosted",
+        "Budget" to "budget",
+        "Room Size" to "roomSize",
+        "Age" to "age",
+        "Name" to "name"
+    )
+    private var selectedOption = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,6 +51,22 @@ class RoommateSearchFragment : Fragment(), RoommateListAdapter.Callbacks {
         auth = FirebaseAuth.getInstance()
 
         setupRecyclerView()
+
+        binding.searchSort.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Sort By")
+                .setSingleChoiceItems(sortOptions, selectedOption) { _, which ->
+                    selectedOption = which
+                }
+                .setPositiveButton("Show Results") { _, _ ->
+                    sortListings(sortMap[sortOptions[selectedOption]]!!)
+//                    setupRecyclerView()
+                }
+                .setNeutralButton("Cancel") { _, _ ->
+                    setupRecyclerView()
+                }
+                .show()
+        }
     }
 
     private fun setupRecyclerView() {
@@ -56,6 +85,25 @@ class RoommateSearchFragment : Fragment(), RoommateListAdapter.Callbacks {
         adapter.startListening()
     }
 
+    private fun sortListings(sortField: String, order: String = "") {
+        val direction = if (order.isNotEmpty() && order == "DESC") {
+            Query.Direction.DESCENDING
+        } else {
+            Query.Direction.ASCENDING
+        }
+
+        val query = db.collection("RoommateListings")
+            .whereNotEqualTo("userID", auth.currentUser!!.uid)
+            .orderBy(sortField, direction)
+
+        val newOptions = FirestoreRecyclerOptions.Builder<RoommateListingData>()
+            .setQuery(query, RoommateListingData::class.java)
+            .build()
+
+        adapter.updateOptions(newOptions)
+        adapter.notifyDataSetChanged()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -70,7 +118,7 @@ class RoommateSearchFragment : Fragment(), RoommateListAdapter.Callbacks {
     }
 
 
-//    private fun setupOptionClickListeners(view: View) {
+    //    private fun setupOptionClickListeners(view: View) {
 //        view.findViewById<TextView>(R.id.textViewViewOption).setOnClickListener {
 //            // Handle View option click
 //        }
@@ -81,4 +129,13 @@ class RoommateSearchFragment : Fragment(), RoommateListAdapter.Callbacks {
 //            // Handle Filter option click
 //        }
 //    }
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
 }
